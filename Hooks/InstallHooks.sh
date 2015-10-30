a123acd5db1962183aea11e0db47f0a9b602d3dd
lavhal <lavdim.halilaj@gmail.com>
2015-10-30 14:56:26 +0100

    rev

--- Hooks/InstallHooks.sh
+++ Hooks/InstallHooks.sh
@@ -4,7 +4,7 @@ do
  if [ $file != "InstallHooks.sh" ]; then
    cp VoColClient/Hooks/$file .git/hooks/$file
     
-    if [[ "$OSTYPE" != "msys" ]]; then
+    if [ "$OSTYPE" != "msys" ]; then
      echo "Please write your admin password to allow execute permission for git hook: "$file
      sudo chmod +x .git/hooks/$file
    fi
